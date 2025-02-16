62f2b386f8735b7f6ee78007799f91bbe0eda193
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-02-16 12:40:46 +0530

    created user-route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,1 +1,28 @@
 const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+const {validationResult} = require('express-validator');
+
+module.exports.registerUser = async(req,res,next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const {firstname,lastname , email , password} = req.body;
+
+    //password hashed from usermodel methods
+    const hashedPassword = await userService.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword
+    }) ;
+
+    //token generation
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({token, user});
+
+}
