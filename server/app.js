f53b7af341fd4a283ca6c762c196ae6fd9ab9578
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-02-16 12:47:50 +0530

    handled errors

--- server/app.js
+++ server/app.js
@@ -3,14 +3,19 @@ const app = express();
 const dotenv = require('dotenv');   
 dotenv.config();
 const cors = require('cors');
-
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 connectToDb();
 
 app.get('/', (req,res) => {
     res.send('Hello World');
 })
+
+app.use('/users',userRoutes);
+
+
 
 module.exports = app;
